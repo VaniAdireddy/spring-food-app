6d9e2c8f4896c296896a557bcaf575cf59010b5d
dnsri <dnsring143@gmail.com>
2024-03-20 11:44:28 +0530

    OAtu added for security with Authentication authorization.

--- src/main/java/com/foodapp/springfoodapp/SpringFoodAppApplication.java
+++ src/main/java/com/foodapp/springfoodapp/SpringFoodAppApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringFoodAppApplication {
-
+// TODO ->https://github.com/Srinu-rj/spring-food-app.git
 	public static void main(String[] args){
 		SpringApplication.run(SpringFoodAppApplication.class, args);
-		System.out.println("spring-boot");
+		System.out.println("SPRING-BOOT-APPLICATION");
 	}
 
 }
