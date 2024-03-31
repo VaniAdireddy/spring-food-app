2b2cfebeea2978c9439de7074a14b5a0dba7ffa7
Srinu-rj <dnsrinu143@gmail.com>
2024-03-31 15:35:08 +0530

    some changes and build jarFiles

---
+++ src/main/java/com/foodapp/springfoodapp/demoGithu/demo.java
@@ -0,0 +1,4 @@
+package com.foodapp.springfoodapp.demoGithu;
+
+public class demo {
+}
--- src/main/java/com/foodapp/springfoodapp/entiry/OrderDetails.java
+++ src/main/java/com/foodapp/springfoodapp/entiry/OrderDetails.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
