170c7955cbeef0a48419bab170f4b137d62f25b9
dnsri <dnsrinu143@gmail.com>
2024-07-28 18:26:42 +0530

    Jwt ,Security Added

--- src/main/java/com/foodapp/springfoodapp/SpringFoodAppApplication.java
+++ src/main/java/com/foodapp/springfoodapp/SpringFoodAppApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //@EnableAsync  //todo for enable -> Thread
 public class SpringFoodAppApplication {
 // TODO ->https://github.com/Srinu-rj/spring-food-app.git
+//   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
 
 	// todo github token  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
 	public static void main(String[] args){
